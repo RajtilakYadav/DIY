ace570150064c3189537afa3e2dc6c982f164d77
Irs444 <irs786had123@gmail.com>
2024-05-03 10:53:26 +0530

    home

--- frontend/src/Component/user/Product.jsx
+++ frontend/src/Component/user/Product.jsx
@@ -42,11 +42,11 @@ const Product = () => {
   // }, [])
 
   const filterByCategory = (category) => {
-       
+
     const filteredProducts = masterList.filter(product => product.title === category);
     setProduct(filteredProducts);
 
-}
+  }
 
 
 
@@ -59,7 +59,7 @@ const Product = () => {
             <h2>Filter</h2>
             <h3 className="my-2 ">Category</h3>
             <div className="d-flex mb-1">
-              <input type="checkbox" className="" id="home" value='a'  onChange={(e) => filterByCategory("Home Decor")}/>
+              <input type="checkbox" className="" id="home" value='a' onChange={(e) => filterByCategory("Home Decor")} />
               <label for='home' className="ms-3 my-1">Home Decor</label>
             </div>
             <div className="d-flex mb-1">
@@ -75,7 +75,7 @@ const Product = () => {
               <label for='gar' className="ms-3 my-1">Gardening</label>
             </div>
 
-           
+
 
           </div>
           <div className="col-md-10">
@@ -100,53 +100,57 @@ const Product = () => {
                   product.map((pro) => {
                     return (
                       <div className="col-md-6 col-lg-3 mb-4 mb-md-0">
-                      <div className="card shadow p-3 mb-5 bg-white rounded w-100">
-                        <img
-                          src={"http://localhost:3000/" + pro.image}
-                          className="card-img-top"
-                          alt="Laptop"
-                          style={{height:150}}
-                        />
-                        <div className="card-body">
-                          <div className="d-flex justify-content-between">
-                            <p className="small">
-                              <a href="#!" className="text-muted">
-                              Handicraft
-                              </a>
-                            </p>
-                            <p className="small text-danger">
-                              
-                            </p>
-                          </div>
-                          <div className="d-flex justify-content-between mb-3">
-                            <h5 className="mb-0">Cat Vase</h5>
-                            <h5 className="text-dark mb-0"><span>₹</span>{pro.price}</h5>
-                          </div>
-                          <div className="d-flex justify-content-between mb-2">
-                            <p className="text-muted mb-0">
+                        <div className="card shadow p-3 mb-5 bg-white rounded w-100">
+                          <img
+                            src={"http://localhost:3000/" + pro.image}
+                            className="card-img-top"
+                            alt="Laptop"
+                            style={{ height: 150 }}
+                          />
+                          <div className="card-body">
+                            <div className="d-flex justify-content-between">
+                              <p className="small">
+                                <a href="#!" className="text-muted">
+                                  {pro.category}
+                                </a>
+                              </p>
+                              <p className="small text-danger">
+
+                              </p>
+                            </div>
+                            <div className="d-flex justify-content-between mb-3">
+                              <h5 className="mb-0">{pro.title}</h5>
+
+                            </div>
+                            <div>
+                              <h5 className="text-dark mb-0">₹<span className="d-flex">{pro.price}</span></h5>
+                            </div>
+
+                            <div className="d-flex justify-content-between mb-2">
+                              {/* <p className="text-muted mb-0">
                               Available: <span className="fw-bold">7</span>
-                            </p>
-                            {/* <div className="ms-auto text-warning">
+                            </p> */}
+                              {/* <div className="ms-auto text-warning">
                               <i className="fas fa-star" />
                               <i className="fas fa-star" />
                               <i className="fas fa-star" />
                               <i className="fas fa-star" />
                               <i className="far fa-star" />
                             </div> */}
+                            </div>
+                          </div>
+                          <div className="card-footer">
+                            <button
+                              disabled={isInCart(pro)}
+                              onClick={(e) => addItemToCart(pro)}
+                              className="btn btn-outline-primary">
+                              <span>
+                                {isInCart(pro) ? "Already Added" : "Add to Cart"}
+                              </span>
+                            </button>
                           </div>
                         </div>
-                        <div className="card-footer">
-                          <button
-                          disabled={isInCart(pro)}
-                          onClick={(e) => addItemToCart(pro)}
-                          className="btn btn-outline-primary">
-                            <span>
-                              {isInCart(pro) ? "Already Added" : "Add to Cart"}
-                            </span>
-                          </button>
-                          </div>
                       </div>
-                    </div>
                     )
                   })
                 }
